99d575b563fd4b2db72ac690a7a67b9c85f6ccb4
Ishita <78365391+isaxena2002@users.noreply.github.com>
2022-10-19 18:58:27 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,22 @@
+const express= require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/", function(req,res){
+
+var w = Number(req.body.w);
+var h = Number(req.body.h);
+var bmi = w/(h*h);
+console.log(bmi);
+
+    res.send("Your BMI is " + bmi);
+})
+
+app.listen(3000, function(){
+    console.log("server is running on port 3k");
+});
